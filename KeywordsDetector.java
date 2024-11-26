1bf7c89b709ac4f0633f9075b90efedf233f8104
Omer <godmega67@gmail.com>
2024-11-26 14:06:40 +0200

    finished

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,17 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        // doing a nested iteration on both arrays
+        // comparing each sentence with each keyword using
+        // contains method
+        for (int i = 0; i < sentences.length; i++) {
+            for (int j = 0; j < keywords.length; j++) {
+                String sentence = sentences[i];
+                String keyword = keywords[j];
+                if (sentence.contains(keyword)) {
+                    System.out.println(sentence);
+                }
+            }
+        }
     }
 }
--- Primes.java
+++ Primes.java
@@ -1,5 +1,30 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int lastN = 10; // Integer.parseInt(args[0]);
+        boolean[] arr = new boolean[lastN + 1];
+        
+        // Initialize all entries as true. A value in arr[i] will 
+        // finally be false if i is not a prime, else true.
+        for (int i = 2; i <= lastN; i++) {
+            arr[i] = true;
+        }
+
+        for (int p = 2; p * p <= lastN; p++) {
+            // If arr[p] is not changed, then it is a prime
+            if (arr[p] == true) {
+                // Update all multiples of p
+                for (int i = p * p; i <= lastN; i += p) {
+                    arr[i] = false;
+                }
+            }
+        }
+
+        // Print all prime numbers
+        for (int i = 2; i <= lastN; i++) {
+            if (arr[i] == true) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
 }
